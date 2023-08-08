2db3dc226205bc5481a320a3b7ced9daa88fc704
Juho Lee <eungseoni@gmail.com>
2023-08-09 01:46:32 +0900

    feat(admin): make login page

--- admin/src/components/Login.tsx
+++ admin/src/components/Login.tsx
@@ -3,8 +3,17 @@ import "./Login.css";
 
 function App() {
   return (
-    <div>
-      <h1>Daegun24 Admin page</h1>
+    <div className="login_main">
+      <h1 className="login_title">Daegun24 Admin page</h1>
+      <div className="login_form">
+        <label>ID</label>
+        <input type="text" />
+        <label className="login_pw_label">Password</label>
+        <input type="password" />
+      </div>
+      <div className="login_explain">
+        <p>Daegun24 admin page login</p>
+      </div>
     </div>
   );
 }
