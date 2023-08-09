ea1e8d49d759d2c54e32834a2cbe99e56afa5861
II-DW <dowon206@kakao.com>
2023-08-10 02:40:31 +0900

    feat(admin): sperate nav.tsx from Main.tsx

---
+++ admin/src/components/nav.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./nav.css";
+import { Link } from "react-router-dom";
+import user_icon from "./img/user-solid.svg";
+import button_icon from "./img/bullhorn-solid.svg";
+
+function App() {
+  return (
+
+      <div className = 'LeftBar'>
+
+        <div className ='Text'>
+          <span className='MainText'> <br /> &nbsp; &nbsp; &nbsp; &nbsp; Admin Page </span>
+        </div>
+        <br />
+        <hr className="bold_line"/>
+        <br />
+
+        <Link to="/from" className="Nav_Link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img className='icon' src={user_icon} alt='icon'></img><span className="Nav_Text"> &nbsp;Members</span></Link><br/>
+        <br />
+        <hr />
+        <br />
+        <Link to="/from" className="Nav_Link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img className='icon' src={button_icon} alt='icon'></img><span className="Nav_Text"> &nbsp;공지사항</span></Link><br/>
+      </div>
+
+  );
+}
+
+export default App;
