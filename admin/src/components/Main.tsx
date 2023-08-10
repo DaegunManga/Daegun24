4bf17b6a69fbb48e369541eb1d8707c53e96f2fc
II-DW <dowon206@kakao.com>
2023-08-11 03:45:33 +0900

    feat(admin): test Rest API

--- admin/src/components/Main.tsx
+++ admin/src/components/Main.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Main.css";
 import Nav from "./nav";
+import axios from "axios";
 
-function App() {
+function App() {  
+
+  axios.post("http://localhost/api/users", { }).then((res) => {
+    console.log(res.data);
+  });
+
+
   return (
     <div className = 'MainPage'>
       <Nav />
