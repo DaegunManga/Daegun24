fe1e8a7f8643f4e138ca9c2885eccbf1d03680f2
lukekeum <lukekeum11@gmail.com>
2023-08-11 12:29:18 +0900

    feat(app): add auth components

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import DaegunLogo from '../components/Auth/DaegunLogo';
+import InputWithLabel from '../components/Auth/InputWithLabel';
+import SubmitButton from '../components/Auth/SubmitButton';
+import {View} from 'react-native';
 
 export default function Login() {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <DaegunLogo />
+      <View>
+        <InputWithLabel label={'이메일'} />
+        <InputWithLabel label={'비밀번호'} />
+        <SubmitButton content={'로그인'} />
+      </View>
+    </SafeAreaView>
+  );
 }
--- app/screens/Register.tsx
+++ app/screens/Register.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import DaegunLogo from '../components/Auth/DaegunLogo';
+import InputWithLabel from '../components/Auth/InputWithLabel';
+import SubmitButton from '../components/Auth/SubmitButton';
+import {View} from 'react-native';
 
 export default function Register() {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <DaegunLogo />
+      <InputWithLabel label={'이메일 (학교이메일)'} />
+      <View>
+        <InputWithLabel label={'이름'} />
+        <InputWithLabel label={'비밀번호'} />
+        <InputWithLabel label={'비밀번호 확인'} />
+        <SubmitButton content={'회원가입'} />
+      </View>
+    </SafeAreaView>
+  );
 }
