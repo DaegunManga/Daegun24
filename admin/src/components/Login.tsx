67c5c2e3e1e9f179627615a283032e783fafa2dc
Juho Lee <eungseoni@gmail.com>
2023-08-12 12:55:09 +0900

    feat(admin): add login api request

--- admin/src/components/Login.tsx
+++ admin/src/components/Login.tsx
@@ -1,39 +1,46 @@
 import React, { useState } from "react";
 import "./Login.css";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   let [id, setId] = useState<string>("");
   let [pw, setPw] = useState<string>("");
   const [button, setButton] = useState(true);
+  const navigate = useNavigate();
 
-  axios.post("http://localhost:3001/login", { setId, setPw }).then((res) => {
-    console.log(res.data);
-  });
+  const handleInputId = (e: any) => {
+    setId(e.target.value);
+  };
+
+  const handleInputPw = (e: any) => {
+    setPw(e.target.value);
+  };
+
+  const onClickLogin = () => {
+    axios
+      .post("http://localhost:8000/users/login/", {
+        username: setId,
+        password: setPw,
+      })
+      .then((res) => {
+        if (res.data.token) {
+          navigate("/Main");
+        }
+      });
+  };
 
   return (
     <div className="login_main">
       <h1 className="login_title">Daegun24 Admin page</h1>
       <div className="login_form">
         <label>ID</label>
-        <input
-          type="text"
-          name="id"
-          value={id}
-          onChange={(e) => {
-            setId(e.target.value);
-          }}
-        />
+        <input type="text" name="id" value={id} onChange={handleInputId} />
         <label className="login_pw_label">Password</label>
-        <input
-          type="text"
-          name="pw"
-          value={pw}
-          onChange={(e) => {
-            setPw(e.target.value);
-          }}
-        />
-        <button className="login_btn">Login</button>
+        <input type="text" name="pw" value={pw} onChange={handleInputPw} />
+        <button className="login_btn" onClick={onClickLogin}>
+          Login
+        </button>
       </div>
       <div className="login_explain">
         <p>Daegun24 admin page login</p>
