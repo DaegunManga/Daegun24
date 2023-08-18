82cb58f8141c52940ed1fc9e209e8d9da4ac09fa
Juho Lee <eungseoni@gmail.com>
2023-08-18 15:39:07 +0900

    feat(admin): design login page

--- admin/src/components/Login.tsx
+++ admin/src/components/Login.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ChangeEvent, KeyboardEvent } from "react";
 
+import logo from "./img/logo_rectangle.png";
+
 function App() {
   let [id, setId] = useState<string>("");
   let [pw, setPw] = useState<string>("");
@@ -38,27 +40,43 @@ function App() {
   };
 
   return (
-    <div className="login_main">
-      <h1 className="login_title">Daegun24 Admin page</h1>
-      <form>
-        <div className="login_form">
-          <label>ID</label>
-          <input type="text" name="id" value={id} onChange={handleId} />
-          <label className="login_pw_label">Password</label>
-          <input
-            type="text"
-            name="pw"
-            value={pw}
-            onChange={handlePw}
-            onKeyDown={handleKeyPress}
-          />
-          <button className="login_btn" onClick={onClickLogin}>
-            Login
-          </button>
+    <div className="login_back">
+      <div className="login_container">
+        <div className="login_logo">
+          <img src={logo} alt="logo" className="login_logo_dg24" />
         </div>
-      </form>
-      <div className="login_explain">
-        <p>Daegun24 admin page login</p>
+        <div className="login_main">
+          <h1 className="login_title">Admin page</h1>
+          <form>
+            <div className="login_form">
+              <label className="login_label">ID</label>
+              <input
+                type="text"
+                name="id"
+                value={id}
+                onChange={handleId}
+                placeholder="ID"
+                className="login_input"
+              />
+              <label className="login_label">Password</label>
+              <input
+                type="text"
+                name="pw"
+                value={pw}
+                onChange={handlePw}
+                placeholder="Password"
+                onKeyDown={handleKeyPress}
+                className="login_input"
+              />
+              <button className="login_btn" onClick={onClickLogin}>
+                Login
+              </button>
+            </div>
+          </form>
+          <div className="login_explain">
+            <p>Daegun24 admin page</p>
+          </div>
+        </div>
       </div>
     </div>
   );
